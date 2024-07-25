bf5848629af8fec009692471b43bd28dd5cf8465
tanner johnson <tannerjohnson08@gmail.com>
2024-07-24 23:38:01 -0500

    still need to figure out backtick escape for md

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const fs = require("fs");
 
 
 // TODO: Create an array of questions for user input
@@ -38,8 +39,14 @@ const questions = [
         type: "list",
         message: "Which license are you using for this project?",
         name: "license",
-        choices: []
+        choices: ["MIT", "IBM", "Apache", "Eclipse", "Boost"]
     },
+    // License badge links respectively according to choices
+// [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+// [![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)
+// [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+// [![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)
+// [![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)    
     {
         type: "input",
         message: "What is your GitHub username?",
@@ -57,25 +64,54 @@ const questions = [
 function writeToFile(fileName, data) {
 
 
+    let fileString = (`
+        # ${data.title}
+
+
+        ## Table of Contents
+        - Descrition
+        - Installation Instructions
+        - Usage Information
+        - License
+        - Contribution Guidelines
+        - Test Instructions
+        - Questions
+
+        ## Description
+        ${data.description}
+
+        ## Installation
+        ${data.installInstructions}
+
+        ## Usage
+        ${data.useInfo}
+
+        ## License
+
+
+        ## Contribution
+        ${data.contribution}
+
+        ## Tests
+        ${data.testInstructions}
+
+        ## Questions
+
+        [GitHub](https://github.com/${data.github})
+        Email: ${data.email}
+            - Feel free to contact me at any time with questions, but please allow a few days for a response.
+        `)
+
+    fs.writeFile(fileName, fileString.raw[0],(err) => err ? console.log(err) : console.log("Success!"));
+
 }
 
 // TODO: Create a function to initialize app
-function init() {
+async function init() {
 
-    let userReadMe = {
-        title: "",
-        description: "",
-        installInstructions: "",
-        useInfo: "",
-        constribution: "",
-        testInstructions: "",
-        license: "",
-        github: "",
-        email: "",
-    }
+    const responses = await inquirer.prompt(questions);
 
-    
-
+    writeToFile("sample-README.md", responses);
 
 }
 
---
+++ sample-README.md
@@ -0,0 +1,37 @@
+
+        # ;laskdjf
+
+
+        ## Table of Contents
+        - Descrition
+        - Installation Instructions
+        - Usage Information
+        - License
+        - Contribution Guidelines
+        - Test Instructions
+        - Questions
+
+        ## Description
+        as;ldkfj
+
+        ## Installation
+        sdaf;lkj
+
+        ## Usage
+        ]sadf;lkj
+
+        ## License
+
+
+        ## Contribution
+        asdf;lkj
+
+        ## Tests
+        asdf'lkj
+
+        ## Questions
+
+        [GitHub](https://github.com/;alskfj)
+        Email: ;laksdfj
+            - Feel free to contact me at any time with questions, but please allow a few days for a response.
+        
